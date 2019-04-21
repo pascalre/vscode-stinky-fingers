e9b91b27d7d79fd59753896da41a71e53646604f
Pascal Reitermann <pascalreitermann+github@gmail.com>
2019-04-22 00:06:31 +0200

    line indentation

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -15,32 +15,33 @@ import { handleTypedChar, dictionary, numberOfCharsUntilBlame, blameText, blameI
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", function () {
 
-    test("Test 1/4: Char should not be replaced.", function() {
-        assert.equal(handleTypedChar('p'), 'p');
-    });
+  test("Test 1/4: Char should not be replaced.", function() {
+    assert.equal(handleTypedChar('p'), 'p');
+  });
 
 
-    test("Test 2/4: Char should be replaced by char from map.", function() {
-        dictionary.set('g', 'h')
-        assert.equal(handleTypedChar('g'), 'h');
-    });
+  test("Test 2/4: Char should be replaced by char from map.", function() {
+    dictionary.set('g', 'h')
+    assert.equal(handleTypedChar('g'), 'h');
+  });
 
 
-    test("Test 3/4: A new char <-> replace entry should be done into map.", function() {
-        var keys = dictionary.size;
-        assert.notEqual(handleTypedChar('h'), 'h');
-        assert.equal(keys+1, dictionary.size);
-    });
+  test("Test 3/4: A new char <-> replace entry should be done into map.", function() {
+    var keys = dictionary.size;
+    assert.notEqual(handleTypedChar('h'), 'h');
+    assert.equal(keys+1, dictionary.size);
+  });
 
-    test("Test 4/4: Char should be replaced by char from blameValue.", function() {
-        dictionary.clear();
-        var i;
-        for (i = 0; i < numberOfCharsUntilBlame; i++) { 
-            dictionary.set(i.toString(), 'x');
-        }
-        console.log("dictionary size: " + dictionary.size);
-        assert.equal(handleTypedChar('g'), "\n" + blameText.charAt(1));
-        assert.equal(handleTypedChar('g'), blameText.charAt(1));
+  test("Test 4/4: Char should be replaced by char from blameValue.", function() {
+    dictionary.clear();
+    var i;
+    // set some random stuff
+    for (i = 0; i < numberOfCharsUntilBlame; i++) { 
+      dictionary.set(i.toString(), 'x');
+    }
 
-    });
+    assert.equal(handleTypedChar('g'), "\n" + blameText.charAt(1));
+    assert.equal(handleTypedChar('g'), blameText.charAt(1));
+
+  });
 });
